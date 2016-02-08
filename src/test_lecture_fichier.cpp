cebb9cf1d39501b15a49a9aecab88452b38a093f
Ayutaya Rattanatray <ayutaya.rattanatray@ensimag.grenoble-inp.fr>
2016-02-08 14:52:09 +0100

    hdihfdih

---
+++ src/test_lecture_fichier.cpp
@@ -0,0 +1,14 @@
+//
+// Created by rattanaa on 08/02/16.
+//
+
+#include <iostream>
+#include "Dvector.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    char *fichier = argv[1];
+    Dvector d5 = Dvector(fichier);
+    d5.display(cout);
+}
